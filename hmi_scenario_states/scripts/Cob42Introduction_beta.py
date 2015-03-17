4c7ed95c6b54348bd6e2a2fa288d44a6ac9d7623
ipa-nhg-sa <nhg-sa@ipa.fhg.de>
2015-03-17 18:00:41 +0100

    add: what to do while charging

--- hmi_scenario_states/scripts/Cob42Introduction_beta.py
+++ hmi_scenario_states/scripts/Cob42Introduction_beta.py
@@ -10,6 +10,11 @@ import random
 from simple_script_server import *
 sss = simple_script_server()
 
+##
+#Missing:
+#Movement of the base: sss.move_base_rel("base", [0.1[m], 0.1[m], 0.1[rad]], True)
+
+## -- Initiation
 class CobIntroductionInit(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
@@ -31,7 +36,7 @@ class CobIntroductionInit(smach.State):
             return "failed"
 
         return "succeeded"
-
+## -- Recover
 class CobIntroductionRecover(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
@@ -54,14 +59,15 @@ class CobIntroductionRecover(smach.State):
 
         return "succeeded"
 
+## -- Prepare
 class CobIntroductionPrepare(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
             outcomes=['succeeded','failed'])
 			
     def execute(self, userdata):
-        handle_arm_left = sss.move("arm_left", "home",False)
-        handle_arm_right = sss.move("arm_right", "home",False)
+        handle_arm_left = sss.move("arm_left", "folded",False)
+        handle_arm_right = sss.move("arm_right", "folded",False)
         handle_sensorring = sss.move("sensorring", "front",False)
 
         handle_arm_left.wait()
@@ -77,16 +83,18 @@ class CobIntroductionPrepare(smach.State):
 
         return "succeeded"
 
+## -- main script
 class CobIntroduction(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
             outcomes=['succeeded','failed'])
 			
     def execute(self, userdata):
-    
+        
+        # :: Menue to select scenes
         while True:
             rospy.loginfo("0 = Introduction")
-            rospy.loginfo("1 = Modules: base")
+            rospy.loginfo("1 = Modules: base & lights")
             rospy.loginfo("2 = Modules: torso")
             rospy.loginfo("3 = Modules: arms")
             usr_input = raw_input("Please type a number(int) for the scene to begin with:")
@@ -94,13 +102,20 @@ class CobIntroduction(smach.State):
         n = int(usr_input)
 		
         if n == 0:
-            #::::::Introduction
+            # :: 0.Introduction
             rospy.loginfo("Beginning introduction of Care-O-Bot 4-2 for HMI 2015")
             sss.move("arm_right","side", False)
             sss.move("arm_left","side")
 
             rospy.loginfo("Scene 1 : Hello and Welcome")
             handle_wave = sss.move("arm_right", "wave_hmi", False)
+            bool_turn = True
+            while False: #bool_turn once movement works
+                sss.move_base_rel("base", [0, 0, 0.3])
+                sss.move_base_rel("base",[0, 0, -0.6])
+                if(handle_wave.get_state("ACTIVE") == 1):
+                    bool_turn = false
+                sss.move_base_rel("base", [0, 0, 0.3])
             sss.say(["Hello and Welcome to my Presentation, my name is Care-O-Bot. Please dont be afraid, i am a peacefull beeing"])
             handle_wave.wait()
             sss.move("arm_right","point2chest")
@@ -110,32 +125,46 @@ class CobIntroduction(smach.State):
             rospy.loginfo("Scene 2 : Count capabilities")
 
             handle_count = sss.move("arm_right", "count", False)
-            sss.say("I can show you my mood and intention with lights and my head-display")
-            sss.say("I am also able to recognize different types of objekts wich i can manipulate and interact with")
-            handle_count.wait()
+            sss.say(["I can show you my mood and intention with lights and my head-display"])
+            sss.say(["I am also able to recognize different types of objekts wich i can manipulate and interact with"])
+            sss.move("arm_right","side")
+            
+            
 			
             n = n+1
 
         if n == 1:
-            #::::::Explain modules (base)
-            rospy.loginfo("Explaining modules(base)")
+            # :: 1.Explain modules (base & lights)
+            rospy.loginfo("Explaining modules(base & lights)")
             sss.say(["For interaction with you and expressing my mood iam able to change my colored lights and use my head-integrated display"])
             # Colors & Mimics / Not done yet
 
             #(...)
         
-            sss.say(["I consist of 4 elementary parts, i'll start with my base."])
-            # Move Base
-            sss.say(["I can move forwar and backward, or to the side"])
-            # Turn Base
+            sss.say(["I consist of 4 elementary parts, i'll start with my base."], False)
+            sss.move("arm_right","point2base")
+            sss.say(["I can move forward and backward"], False)
+            sss.move_base_rel("base",[-0.1, 0, 0])
+            sss.move_base_rel("base",[0.1, 0, 0])
+            sss.say(["or sideways"])
+            sss.move_base_rel("base",[0, 0.1, 0])
+            sss.move_base_rel("base",[0, -0.1, 0])
+            sss.move_base_rel("base",[0, -0.1, 0], False)
+            sss.say(["and back."], False)
+            sss.move_base_rel("base",[0, 0.1, 0])
             sss.say(["Iam also capable to turn on the spot like this"])
-            # Turn and Move Base
+            sss.move_base_rel("base",[0, 0, 0.1])
+            sss.move_base_rel("base",[0, 0, 0.1])
+            sss.move_base_rel("base",[0, 0, -0.1])
+            sss.move_base_rel("base",[0, 0, -0.1])
+            sss.move_base_rel("base",[0, 0, -0.1])
+            sss.move_base_rel("base",[0, 0, 0.1])
             sss.say(["And, of course, i can combine the movements"])
 
             n = n+1
         
         if n == 2:
-            #::::::Explain modules (torso)
+            # :: 2.Explain modules (torso)
             rospy.loginfo("Explaining modules(torso)")
             sss.move("arm_right","point2chest")
             handle_drawtorso = sss.move("arm_right","draw_torso", False)
@@ -145,13 +174,17 @@ class CobIntroduction(smach.State):
             n = n+1
         
         if n == 3:
-			#::::::Explain modules (arms)
+			# :: 3.Explain modules (arms)
             rospy.loginfo("Explaining modules(arms)")
-            sss.move("arm_right", "home", False)
-            sss.move("arm_left", "home", False)
+            sss.move("arm_right",[[1.5, 0, 0, 0, 0, 0, 0]],False)
+            sss.move("arm_left",[[-1.5, 0, 0, 0, 0, 0, 0]])
             sss.say(["as you can see, i have two arms, which i can use indepently"])
-            #sss.move("arm_right", "rotate", False)
-            #sss.move("arm_left", "inv_rotate", False)
+            r_handle = sss.move("arm_right",[[1.5, 0.3, 0, 0, 0, 0, 0]], False)
+            sss.move("arm_left",[[-1.5, 0.3, 0, 0, 0, 0, 0]])           
+            r_handle.wait()
+            sss.move("arm_right",[[1.5, -0.3, 0, 0, 0, 0, 0]], False)
+            sss.move("arm_left",[[-1.5, -0.3, 0, 0, 0, 0, 0]])
+            
             sss.say(["Both arms consist of 7 independet joints, allowing me to perform complex movements"], False)
 
             rospy.loginfo("Beginning arena-like wave")
@@ -173,9 +206,12 @@ class CobIntroduction(smach.State):
             n = n+1
 
 
+        ## :: Final
         
 
         return 'succeeded'
+
+## -- State Machine 
 
 class Explore(smach.StateMachine):
     def __init__(self):
